b6d9dd25db7a8438d4dba65f0124e7fcfa5b5a86
albert831229 <albertyang1994@gmail.com>
2018-10-16 01:33:36 -0700

    Optimize with caching

--- bravado_core/unmarshal.py
+++ bravado_core/unmarshal.py
@@ -42,7 +42,7 @@ def unmarshal_schema_object(swagger_spec, schema_object_spec, value):
     else:
         deref = swagger_spec.deref
     schema_object_spec = deref(schema_object_spec)
-    #print(schema_object_spec)
+    print(schema_object_spec)
     #print(deref.cache_info())
 
     obj_type = schema_object_spec.get('type')
