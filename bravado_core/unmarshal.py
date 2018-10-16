e11c6a4cb963ce83328bf3bdcb47d1fea7fb4d35
albert831229 <albertyang1994@gmail.com>
2018-10-16 00:40:44 -0700

    Optimize with caching

--- bravado_core/unmarshal.py
+++ bravado_core/unmarshal.py
@@ -179,7 +179,6 @@ def unmarshal_model(swagger_spec, model_spec, model_value):
     #if not is_frozendict_like(model_spec) and is_dict_like(model_spec):
     #    model_spec = transform_dict_to_frozendict(model_spec)
     if not is_frozendict_like(model_spec):
-        print('ttt')
         model_spec = frozendict(model_spec)
     deref = swagger_spec.fast_deref
     #print(deref.cache_info())
