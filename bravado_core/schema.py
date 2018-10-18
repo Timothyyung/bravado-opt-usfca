7137539cd353db7293362bf553ac82ede0be856f
albert831229 <albertyang1994@gmail.com>
2018-10-17 19:42:10 -0700

    Optimize with caching

--- bravado_core/schema.py
+++ bravado_core/schema.py
@@ -216,7 +216,7 @@ def collapsed_properties(model_spec, swagger_spec):
 
     # allOf may or may not be present
     if 'allOf' in model_spec:
-        deref = swagger_spec.deref
+        deref = swagger_spec.fast_deref
         for item_spec in model_spec['allOf']:
             item_spec = deref(item_spec)
             more_properties = collapsed_properties(item_spec, swagger_spec)
