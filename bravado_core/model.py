a54bc30b8840c17fcc53615ee621fc6206117e3e
albert831229 <albertyang1994@gmail.com>
2018-10-17 21:11:03 -0700

    Optimize with caching

--- bravado_core/model.py
+++ bravado_core/model.py
@@ -611,6 +611,7 @@ def is_model(swagger_spec, schema_object_spec):
     """
     deref = swagger_spec.deref
     schema_object_spec = deref(schema_object_spec)
+    print(schema_object_spec)
     return deref(schema_object_spec.get(MODEL_MARKER)) is not None
 
 
