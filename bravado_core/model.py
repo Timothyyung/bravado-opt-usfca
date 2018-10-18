a431441c740f3d4f62a567efb082ff577f7fcbfd
albert831229 <albertyang1994@gmail.com>
2018-10-17 22:22:56 -0700

    Optimize with caching

--- bravado_core/model.py
+++ bravado_core/model.py
@@ -588,7 +588,7 @@ def create_model_type(swagger_spec, model_name, model_spec, bases=(Model,), json
     if 'allOf' in model_spec:
         print('1')
         for schema in model_spec['allOf']:
-            inherited_name = swagger_spec.fast_deref(schema).get(MODEL_MARKER, None)
+            inherited_name = swagger_spec.deref(schema).get(MODEL_MARKER, None)
             print(inherited_name)
             if inherited_name:
                 inherits_from.append(inherited_name)
