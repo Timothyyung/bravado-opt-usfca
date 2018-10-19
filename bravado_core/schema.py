e23a1c7cb62601b073af6db6cffa153a45c90a9f
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2018-10-18 18:52:23 -0700

    test2

--- bravado_core/schema.py
+++ bravado_core/schema.py
@@ -168,25 +168,24 @@ def collapsed_properties(model_spec, swagger_spec):
     :returns: dict
     """
     # spec = model_spec
-    # i = id(spec)
-    # try:
-    #     return cache[i]
-    # except KeyError:
+    i = id(model_spec)
+    try:
+        return cache[i]
+    except KeyError:
+        properties = {}
 
-    properties = {}
+        # properties may or may not be present
+        if 'properties' in model_spec:
+            for attr, attr_spec in iteritems(model_spec['properties']):
+                properties[attr] = attr_spec
 
-    # properties may or may not be present
-    if 'properties' in model_spec:
-        for attr, attr_spec in iteritems(model_spec['properties']):
-            properties[attr] = attr_spec
+        # allOf may or may not be present
+        if 'allOf' in model_spec:
+            deref = swagger_spec.deref
+            for item_spec in model_spec['allOf']:
+                item_spec = deref(item_spec)
+                more_properties = collapsed_properties(item_spec, swagger_spec)
+                properties.update(more_properties)
 
-    # allOf may or may not be present
-    if 'allOf' in model_spec:
-        deref = swagger_spec.deref
-        for item_spec in model_spec['allOf']:
-            item_spec = deref(item_spec)
-            more_properties = collapsed_properties(item_spec, swagger_spec)
-            properties.update(more_properties)
-
-    # cache[i] = properties
-    return properties
+        cache[i] = properties
+        return properties
