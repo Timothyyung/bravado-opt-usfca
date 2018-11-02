c191c799b2e7fe3e0026e9aac6f35e906854d7f0
albert831229 <albertyang1994@gmail.com>
2018-11-01 23:57:54 -0700

    Optimize with caching

--- bravado_core/schema.py
+++ bravado_core/schema.py
@@ -100,7 +100,8 @@ def get_spec_for_prop(swagger_spec, object_spec, object_value, prop_name, proper
     if properties is None:
         properties = collapsed_properties(deref(object_spec), swagger_spec)
     prop_spec = properties.get(prop_name)
-    print(prop_name + '       ' + prop_spec)
+    print(prop_name)
+    print(prop_spec)
 
     if prop_spec is not None:
         result_spec = deref(prop_spec)
