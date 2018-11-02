b4616ef7eaca95944b583588c6ee6e76a39dc073
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2018-11-02 16:00:00 -0700

    refactor

--- bravado_core/unmarshal.py
+++ bravado_core/unmarshal.py
@@ -32,7 +32,9 @@ def unmarshal_schema_object(swagger_spec, schema_object_spec, value):
     """
     deref = swagger_spec.deref
     schema_object_spec = deref(schema_object_spec)
+    unmarshal_schema_object_sub(swagger_spec, schema_object_spec, value)
 
+def unmarshal_schema_object_sub(swagger_spec, schema_object_spec, value):
     obj_type = schema_object_spec.get('type')
 
     if 'allOf' in schema_object_spec:
@@ -52,45 +54,6 @@ def unmarshal_schema_object(swagger_spec, schema_object_spec, value):
 
     if swagger_spec.config['use_models'] and \
             is_model(swagger_spec, schema_object_spec):
-        # It is important that the 'model' check comes before 'object' check.
-        # Model specs also have type 'object' but also have the additional
-        # MODEL_MARKER key for identification.
-        return unmarshal_model(swagger_spec, schema_object_spec, value)
-
-    if obj_type == 'object':
-        return unmarshal_object(swagger_spec, schema_object_spec, value)
-
-    if obj_type == 'file':
-        return value
-
-    raise SwaggerMappingError(
-        "Don't know how to unmarshal value {0} with a type of {1}"
-        .format(value, obj_type))
-
-def unmarshal_schema_object_array(swagger_spec, schema_object_spec, value):
-
-    obj_type = schema_object_spec.get('type')
-
-    if 'allOf' in schema_object_spec:
-        obj_type = 'object'
-
-    if not obj_type:
-        if swagger_spec.config['default_type_to_object']:
-            obj_type = 'object'
-        else:
-            return value
-
-    if obj_type in SWAGGER_PRIMITIVES:
-        return unmarshal_primitive(swagger_spec, schema_object_spec, value)
-
-    if obj_type == 'array':
-        return unmarshal_array(swagger_spec, schema_object_spec, value)
-
-    if swagger_spec.config['use_models'] and \
-            is_model(swagger_spec, schema_object_spec):
-        # It is important that the 'model' check comes before 'object' check.
-        # Model specs also have type 'object' but also have the additional
-        # MODEL_MARKER key for identification.
         return unmarshal_model(swagger_spec, schema_object_spec, value)
 
     if obj_type == 'object':
