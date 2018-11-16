4d67de7b53638ebf636f21cfbd8c37dbb913e7c2
QiyanYu <qyu18@dons.usfca.edu>
2018-11-16 10:42:09 -0800

    optimize unmarshal_object

--- bravado_core/schema.py
+++ bravado_core/schema.py
@@ -84,7 +84,7 @@ def is_list_like(spec):
 
 
 def get_spec_for_prop(swagger_spec, object_spec, object_value, prop_name, deref,
-  properties=None):
+  is_additional_props_bool, is_additional_props_dict,additional_props, properties=None):
     """Given a jsonschema object spec and value, retrieve the spec for the
      given property taking 'additionalProperties' into consideration.
 
@@ -117,17 +117,17 @@ def get_spec_for_prop(swagger_spec, object_spec, object_value, prop_name, deref,
             result_spec['x-nullable'] = prop_spec['x-nullable']
         return result_spec
 
-    additional_props = deref(object_spec).get('additionalProperties', True)
+    #additional_props = deref(object_spec).get('additionalProperties', True)
 
-    if isinstance(additional_props, bool):
-    #if is_additional_props_bool:
+    #if isinstance(additional_props, bool):
+    if is_additional_props_bool:
         # no spec for additional properties to conform to - this is basically
         # a way to send pretty much anything across the wire as is.
         return None
 
-    additional_props = deref(additional_props)
-    if is_dict_like(additional_props):
-    #if is_additional_props_dict:
+    #additional_props = deref(additional_props)
+    #if is_dict_like(additional_props):
+    if is_additional_props_dict:
         # spec that all additional props MUST conform to
         return additional_props
 
