c17e84b157ad33b5157665531ade82816462b0d5
QiyanYu <qyu18@dons.usfca.edu>
2018-11-19 16:06:33 -0800

    cython

--- setup.py
+++ setup.py
@@ -51,7 +51,10 @@ setup(
     extras_require={
         ':python_version<"3.4"': ['enum34'],
     },
-    ext_modules = [Extension("bravado_core.model", ["bravado_core/model.c"]),
-    Extension("bravado_core.schema", ["bravado_core/schema.c"])],
+    ext_modules = [
+        Extension("bravado_core.model", ["bravado_core/model.c"]),
+        Extension("bravado_core.schema", ["bravado_core/schema.c"]),
+        Extension("bravado_core.spec", ["bravado_core/spec.c"]),
+    ],
     
 )
